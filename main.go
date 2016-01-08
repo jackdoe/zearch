70eda380f99e3d9e76b2041a2e43a53b19e925d3
borislav nikolov <jack@sofialondonmoskva.com>
2016-01-08 22:57:02 +0100

    zearch.io: list indexed repos; add fork me on github ribbon; remove jquery dependency

--- main.go
+++ main.go
@@ -136,32 +136,45 @@ func main() {
 		w.Header().Set("Content-Type", "text/html")
 		s := `
 <html><head><title>zearch</title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-<script>
-$(document).ready(function() {
-   var work = function(q) {
-        $('#res').html("")
-
-        s = ""
-        $.get('/search?' + q, function(data) {
-            s += "took: " + data.TookSeconds.toFixed(5) + "s, matching: " + data.FilesMatching + ", searched in " + data.FilesInIndex + " files and " + data.TokensInIndex + " tokens\n"
-            for (var i = 0; i < data.Hits.length; i++) {
-                var hit = data.Hits[i]
-                s +=  hit.Score + " <a href='/fetch?"+hit.Id +"#"+hit.Path+"'>"+hit.Path+"</a>\n"
-            }
-            $('#res').html(s)
-            window.location.hash = q
-        })
-    }
-    $("#q").keyup(function() { work($(this).val()); })
-    $("#q").val(window.location.hash.substr(1))
-    work($("#q").val())
-})
-</script></head>
+</head>
 <body>
-<input id=q autofocus><br>
+<a href="https://github.com/jackdoe/zearch"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
+<input id=q autofocus>&nbsp;<small>zearch.io: go linux jdk8 glibc curator hadoop hbase kafka log4j2 lucene-solr mesos musl perl5 spark</small><br>
 <pre id=res></pre>
 </body>
+<script>
+var res = document.getElementById("res")
+var work = function(query) {
+    var s = ""
+    res.innerHTML = s
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/search?' + query);
+    xhr.send(null);
+    xhr.onreadystatechange = function () {
+       if (xhr.readyState === 4) {
+           if (xhr.status === 200) {
+               data = JSON.parse(xhr.responseText);
+               s += "took: " + data.TookSeconds.toFixed(5) + "s, matching: " + data.FilesMatching + ", searched in " + data.FilesInIndex + " files and " + data.TokensInIndex + " tokens\n"
+               for (var i = 0; i < data.Hits.length; i++) {
+                   var hit = data.Hits[i]
+                   s +=  hit.Score + " <a href='/fetch?"+hit.Id +"#"+hit.Path+"'>"+hit.Path+"</a>\n"
+               }
+               res.innerHTML = s
+               window.location.hash = query
+           } else {
+               res.innerHTML = "error fetching results, status:" + xhr.status + ", text: xhr.responseText"
+           }
+       }
+   }
+}
+
+var q = document.getElementById("q")
+q.addEventListener('keyup', function(event) { work(q.value) });
+q.value = window.location.hash.substr(1)
+if (q.value > 0)
+        work(q.value)
+</script>
 </html>`
 		fmt.Fprintf(w, s)
 	})
